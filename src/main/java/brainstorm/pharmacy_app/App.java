f709a1ee09ba16d012d04671b74ffd1d3d01c4ff
Taz <moetezzouari1@gmail.com>
2026-01-15 14:49:04 +0100

    Initial commit

---
+++ src/main/java/brainstorm/pharmacy_app/App.java
@@ -0,0 +1,13 @@
+package brainstorm.pharmacy_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
