62ad2a201cb4ece13bfac667c21cfafbb08705da
niniblade <nini.dahaka@gmail.com>
2026-01-15 23:23:32 +0100

    badalt _bd

---
+++ src/main/java/brainstorm/pharmacy_app/Employé.java
@@ -0,0 +1,9 @@
+package brainstorm.pharmacy_app;
+
+public class Employé extends personne {
+    public string Nom_utilisateur;
+    private string Mot_de_passe;
+    protected string Role;
+    protected string Horaire;
+
+}
---
+++ src/main/java/brainstorm/pharmacy_app/personne.java
@@ -0,0 +1,8 @@
+package brainstorm.pharmacy_app;
+
+public class personne {
+    public int id_Personne;
+    public string Nom,Prénom;
+    public int Num_Téléphone;
+    public string E-mail;
+}
