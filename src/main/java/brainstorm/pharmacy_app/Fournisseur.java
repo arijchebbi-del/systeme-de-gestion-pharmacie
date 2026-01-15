a19282df904d7f0ac53dd8d2dcf91a28bd4a859f
Taz <moetezzouari1@gmail.com>
2026-01-16 00:53:43 +0100

    Minor fixes

---
+++ src/main/java/brainstorm/pharmacy_app/Fournisseur.java
@@ -0,0 +1,10 @@
+package brainstorm.pharmacy_app;
+
+public class Fournisseur {
+    private int id_Fournisseur;
+    private String nom;
+    private String numTelephone;
+    private String email;
+    private String adresse;
+    private String typeProduits;
+}
