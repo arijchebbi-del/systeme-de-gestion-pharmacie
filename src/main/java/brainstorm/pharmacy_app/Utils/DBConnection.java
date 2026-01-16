b7b1edb0e0bf726f33ff1f4e5196de139ddb2481
Taz <moetezzouari1@gmail.com>
2026-01-17 00:55:17 +0100

    Change DBConnection Passwords

--- src/main/java/brainstorm/pharmacy_app/Utils/DBConnection.java
+++ src/main/java/brainstorm/pharmacy_app/Utils/DBConnection.java
@@ -7,14 +7,17 @@ import java.sql.SQLException;
 public class DBConnection {
 	//To make a connection you need create instance of DBConnection with user
 	private static String  URL = "jdbc:mysql://127.0.0.1:3306/pharmacy_db";
-	private static String ADMIN_USER="admin";
 	private static String EMP_USER="employee";
-	private static String PASSWORD = "password123";
+	private static String EMP_PASSWORD = "employee123";
+	private static String ADMIN_USER="admin";
+	private static String ADMIN_PASSWORD = "admin123";
+
+
 
 	public static Connection getEmployeeConnection() throws SQLException {
-		return DriverManager.getConnection(URL, EMP_USER, PASSWORD);
+		return DriverManager.getConnection(URL, EMP_USER, EMP_PASSWORD);
 	}
 	public static Connection getAdminConnection() throws SQLException {
-		return DriverManager.getConnection(URL, ADMIN_USER, PASSWORD);
+		return DriverManager.getConnection(URL, ADMIN_USER, ADMIN_PASSWORD);
 	}
 }
