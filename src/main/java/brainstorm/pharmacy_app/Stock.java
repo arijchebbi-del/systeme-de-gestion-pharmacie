01783d59b13709aa7bd15791f8fba30a00386214
arij chebbi <arij.chebbi@insat.ucar.tn>
2026-01-16 17:03:37 +0100

    started stock :)

--- src/main/java/brainstorm/pharmacy_app/Stock.java
+++ src/main/java/brainstorm/pharmacy_app/Stock.java
@@ -1,7 +1,19 @@
 package brainstorm.pharmacy_app;
+import java.time.LocalDate
 
 public class Stock {
-    private int idStock;
-    private String derniereMiseAJour;
+    private int numLot;
+    private LocalDate derniereMiseAJour;
     private int quantite;
+    private int seuilMinimal;
+    private Produit produit;
+    public Stock(int numLot, Produit produit, int quantite, int seuilMinimal) {
+        this.numLot = numLot;
+        this.produit = produit;
+        this.quantite = quantite;
+        this.seuilMinimal = seuilMinimal;
+        this.derniereMiseAJour = LocalDate.now();
+    }
+    
+
 }
