a022b90cd54b2a9748ea0651a85813be1a91ad1b
niniblade <nini.dahaka@gmail.com>
2026-01-16 22:16:25 +0100

    badalt fournisseur

--- src/main/java/brainstorm/pharmacy_app/Fournisseur.java
+++ src/main/java/brainstorm/pharmacy_app/Fournisseur.java
@@ -7,4 +7,20 @@ public class Fournisseur {
     private String email;
     private String adresse;
     private String typeProduits;
+
+    //Getters
+    public int getId_Fournisseur() {return id_Fournisseur;}
+    public String getNom() {return nom;}
+    public String getNumTelephone() {return numTelephone;}
+    public String getEmail() {return email;}
+    public String getAdresse() {return adresse;}
+    public String getTypeProduits() {return typeProduits;}
+
+    //Setters
+    public void setId_Fournisseur(int id_Fournisseur) {this.id_Fournisseur = id_Fournisseur;}
+    public void setNom(String nom) {this.nom = nom;}
+    public void setNumTelephone(String numTelephone) {this.numTelephone = numTelephone;}
+    public void setEmail(String email) {this.email = email;}
+    public void setAdresse(String adresse) {this.adresse = adresse;}
+    public void setTypeProduits(String typeProduits) {this.typeProduits = typeProduits;}
 }
