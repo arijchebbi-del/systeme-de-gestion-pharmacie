6113737bff3b82b33643fc55ee87c96e4d75413e
niniblade <nini.dahaka@gmail.com>
2026-01-20 00:20:45 +0100

    dashboard

--- src/main/java/brainstorm/pharmacy_app/controller/DashboardController.java
+++ src/main/java/brainstorm/pharmacy_app/controller/DashboardController.java
@@ -1,7 +1,81 @@
 package brainstorm.pharmacy_app.controller;
 
+import brainstorm.pharmacy_app.nav.Navigation;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardController {
+    @FXML
+    private void chargerDashboard(ActionEvent event) {
+        Navigation.navTo("/FXML/Dashboard.fxml",((Node) event.getSource())); //charger dashboard
+    }
+    @FXML
+    private void chargerPointOfSale(ActionEvent event) {
+        Navigation.navTo("/FXML/PointOfSale.fxml",((Node) event.getSource())); //charger dashboard
+    }
+    @FXML
+    private void chargerProductControl(ActionEvent event) {
+        Navigation.navTo("/FXML/ProductControl.fxml",((Node) event.getSource())); //charger dashboard
+    }
+    @FXML
+    private void chargerOrderControl(ActionEvent event) {
+        Navigation.navTo("/FXML/OrderControl.fxml",((Node) event.getSource())); //charger dashboard
+    }
+    @FXML
+    private void chargerSuppliersControl(ActionEvent event) {
+        Navigation.navTo("/FXML/SuppliersControl.fxml",((Node) event.getSource())); //charger dashboard
+    }
+    @FXML
+    private void chargerHistory(ActionEvent event) {
+        Navigation.navTo("/FXML/History.fxml",((Node) event.getSource())); //charger dashboard
+    }
+    @FXML
+    private void handleAddProduct(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/AddProductPopUp.fxml"));
+            Scene scene = new Scene(loader.load());
 
-}
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Ajouter Nouveau Produit");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initOwner(((Node) event.getSource()).getScene().getWindow());
+
+            stage.showAndWait();
+
+            // OPTIONAL: refresh dashboard stats here
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private void handleAddSupplier(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/AddSupplierPopUp.fxml"));
+            Scene scene = new Scene(loader.load());
+
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Ajouter Nouveau Produit");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initOwner(((Node) event.getSource()).getScene().getWindow());
+
+            stage.showAndWait();
+
+            // OPTIONAL: refresh dashboard stats here
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+}}
+  
