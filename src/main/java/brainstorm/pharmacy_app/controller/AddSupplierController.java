807261bb62de03167395b6fe852f4b1546a6dd71
arij chebbi <arij.chebbi@insat.ucar.tn>
2026-01-20 12:21:08 +0100

    add supp i addes l handle cancel handle save

--- src/main/java/brainstorm/pharmacy_app/controller/AddSupplierController.java
+++ src/main/java/brainstorm/pharmacy_app/controller/AddSupplierController.java
@@ -2,10 +2,13 @@ package brainstorm.pharmacy_app.controller;
 
 import brainstorm.pharmacy_app.Model.Fournisseur;
 import brainstorm.pharmacy_app.Service.FournisseurService;
-import brainstorm.pharmacy_app.controller.SuppliersControlController;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+
 
 public class AddSupplierController {
 
@@ -22,29 +25,42 @@ public class AddSupplierController {
         this.parentController = parent;
     }
 
+
+    @FXML private TextField nomField;
+    @FXML private TextField telephoneField;
+    @FXML private TextField emailField;
+    @FXML private TextField adresseField;
+
     @FXML
-    private void handleSave() {
-        Fournisseur f = new Fournisseur();
-        f.setNom(txtNom.getText());
-        f.setNumTelephone(txtTel.getText());
-        f.setEmail(txtEmail.getText());
-        f.setAdresse(txtAdresse.getText());
-        f.setTypeProduits(txtType.getText());
+    private void handleSaveSupplier(ActionEvent event) {
+        try {
+            Fournisseur f = new Fournisseur();
+            f.setNom(nomField.getText());
+            f.setNumTelephone(telephoneField.getText());
+            f.setEmail(emailField.getText());
+            f.setAdresse(adresseField.getText());
+            
+            fournisseurService.ajouterFournisseur(f);
 
-        fournisseurService.ajouterFournisseur(f);
+            ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
 
-        if (parentController != null) {
-            parentController.refreshTable();
+        } catch (Exception e) {
+            showError("Erreur", e.getMessage());
         }
-
-        Stage stage = (Stage) txtNom.getScene().getWindow();
-        stage.close();
     }
-
     @FXML
-    private void handleCancel() {
-        Stage stage = (Stage) txtNom.getScene().getWindow();
-        stage.close();
+    private void handleCancel(ActionEvent event) {
+        ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
     }
+
+
+    private void showError(String title, String msg) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(msg);
+        alert.showAndWait();
+    }
+
+
 }
 
